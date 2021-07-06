5d2996c2312ce19f87421d988a984049ef43d044
hamedghd <h.gharibdoost@gmail.com>
2021-07-06 08:55:46 +0200

    Create main-view component.

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from 'ract';
+export class MainView extends React.Component {
+  render() {
+    return (
+      <div className="main-view">
+        <div>The Godfather</div>
+        <div>The Godfather: Part II</div>
+        <div>The Dark Knight</div>
+      </div>
+
+    );
+  }
+}
