726fce54617550ff5641c10a79432b77184b413c
hamedghd <h.gharibdoost@gmail.com>
2021-07-07 12:48:45 +0200

    Add a movies state

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 export class MainView extends React.Component {
+  // Movies state will hold the list of movies.
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: 'The Godfather', Description: 'desc1...', ImagePath: '...' },
+        { _id: 2, Title: 'The Godfather: Part II', Description: 'desc2...', ImagePath: '...' },
+        { _id: 3, Title: 'The Dark Knight', Description: 'desc3...', ImagePath: '...' }
+      ]
+    }
+  }
   render() {
     return (
       <div className="main-view">
