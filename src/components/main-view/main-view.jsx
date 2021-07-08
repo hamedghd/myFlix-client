5452b359816bbeaed1eb575f9e02909ac883bc95
hamedghd <h.gharibdoost@gmail.com>
2021-07-08 12:12:54 +0200

    Add some movie data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,9 +7,24 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'The Godfather', Description: 'desc1...', ImagePath: '...' },
-        { _id: 2, Title: 'The Godfather: Part II', Description: 'desc2...', ImagePath: '...' },
-        { _id: 3, Title: 'The Dark Knight', Description: 'desc3...', ImagePath: '...' }
+        {
+          _id: 1,
+          Title: 'The Godfather',
+          Description: "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
+          ImagePath: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg'
+        },
+        {
+          _id: 2,
+          Title: 'The Godfather: Part II',
+          Description: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
+          ImagePath: 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg'
+        },
+        {
+          _id: 3,
+          Title: 'The Dark Knight',
+          Description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+          ImagePath: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+        }
       ],
       // adds a new state variable to identify whether there was a user click or not.
       selectedMovie: null
