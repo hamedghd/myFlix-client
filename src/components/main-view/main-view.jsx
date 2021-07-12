cd6616dc2a8dab99a5e96cd9cb30e953a4764474
hamedghd <h.gharibdoost@gmail.com>
2021-07-12 22:47:34 +0200

    Add a condition for rendering the login view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -43,11 +43,15 @@ export class MainView extends React.Component {
   // Uses MainView's state to control its UI.
   render() {
     const { movies, selectedMovie } = this.state;
-    // If the movies state array be empty, it doesn't display anything.
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded.
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
+        {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned */}
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
