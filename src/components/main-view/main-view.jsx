8aa545a0665846a5bc8f35b369f05002ae103a49
hamedghd <h.gharibdoost@gmail.com>
2021-07-17 16:54:28 +0200

    Change the Bootstrap stylings

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -68,16 +68,18 @@ export class MainView extends React.Component {
         {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned */}
         {selectedMovie
           ? (
-            <Row className="justify-content-md-center">
-              <Col xs={12} sm={10} md={8} lg={6}>
+            //<Row className="justify-content-md-center ">
+            <Row className="d-flex flex-column justify-content-between align-items-center ">
+
+              <Col xs={12} sm={10} md={8} className="d-flex flex-column align-items-center">
                 <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
               </Col>
             </Row>
           )
           : (
-            <Row className="justify-content-md-center">
+            <Row className="d-flex justify-content-between align-items-center">
               {movies.map(movie => (
-                <Col xs={12} sm={6} md={3}>
+                <Col xs={12} sm={6} md={4} lg={3} className="d-flex flex-column align-items-center">
                   <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
                 </Col>
               ))}
