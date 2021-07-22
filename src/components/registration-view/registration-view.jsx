b1161c458fc98e6ed4b62563af0d4ef8882af414
hamedghd <h.gharibdoost@gmail.com>
2021-07-22 15:24:40 +0200

    Add routing

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -4,6 +4,8 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './registration-view.scss';
 export function RegistrationView(props) {
@@ -16,51 +18,52 @@ export function RegistrationView(props) {
     alert('Thanks for signing up. You can log in now!');
     e.preventDefault();
     console.log(username, password, email, birthday);
-    let register = true;
     /* Send a request to the server for authentication */
-    /* then call props.onRegister(username) */
-    props.onRegister(register);
-  };
+    axios.post('https://api-myflix.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday,
+    })
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+      })
+      .catch((e) => {
+        console.log('error registering the user');
+      });
 
-  return (
-    <Row className="justify-content-md-center">
-      <Col md={6} lg={4} className="registration-style bg-dark">
-        <h1 className="title">Sign up to myFlix!</h1>
-        <Form>
-          <Form.Group controlId="formUsername" >
-            <Form.Label>Username:</Form.Label>
-            <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-          </Form.Group>
+    return (
+      <Row className="justify-content-md-center">
+        <Col md={6} lg={4} className="registration-style bg-dark">
+          <h1 className="title">Sign up to myFlix!</h1>
+          <Form>
+            <Form.Group controlId="formUsername" >
+              <Form.Label>Username:</Form.Label>
+              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
 
-          <Form.Group controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-          </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
 
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
-          </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
+            </Form.Group>
 
-          <Form.Group controlId="formBirthday">
-            <Form.Label>Birthday:</Form.Label>
-            <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
-          </Form.Group>
+            <Form.Group controlId="formBirthday">
+              <Form.Label>Birthday:</Form.Label>
+              <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
+            </Form.Group>
 
-          <Button className="button-style" variant="primary" type="submit" onClick={handleSubmit}>
-            Submit
-          </Button>
-        </Form>
-      </Col>
-    </Row>
-  );
-}
-RegistrationView.propTypes = {
-  register: PropTypes.shape({
-    Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired,
-    Email: PropTypes.string.isRequired,
-    Birthday: PropTypes.date
-  }),
-  onRegister: PropTypes.func.isRequired,
-};
+            <Button className="button-style" variant="primary" type="submit" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
