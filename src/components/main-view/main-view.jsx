3618c83219b2f9c572a9bfaa57a495bda9d5d2cc
hamedghd <h.gharibdoost@gmail.com>
2021-07-22 15:28:56 +0200

    Insert the header directly inside the main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { HeaderView } from '../header-view/header-view';
+import { DirectorView } from '../director-view/director-view';
+import { GenreView } from '../genre-view/genre-view';
+import { ProfileView } from '../profile-view/profile-view';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import imageurl from "url:../../img/myflix.png";
+import Button from 'react-bootstrap/Button';
 
 import './main-view.scss';
 export class MainView extends React.Component {
@@ -16,12 +26,8 @@ export class MainView extends React.Component {
     this.state = {
       // Sets movies state to an empty array.
       movies: [],
-      // adds a new state variable to identify whether there was a user click or not.
-      selectedMovie: null,
       // Sets initial value for user state to null
-      user: null,
-      // Sets initial value for register state to null
-      register: true
+      user: null
     }
   }
   componentDidMount() {
@@ -35,12 +41,6 @@ export class MainView extends React.Component {
       // Queries my myFlix API server’s /movies endpoint with a get request using Axios:
       this.getMovies(accessToken);
     }
-  }
-  /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie */
-  setSelectedMovie(newSelectedMovie) {
-    this.setState({
-      selectedMovie: newSelectedMovie
-    });
   }
 
   getMovies(token) {
@@ -74,52 +74,132 @@ export class MainView extends React.Component {
       user: null
     });
   }
-  onRegister(register) {
-    this.setState({
-      register,
-    });
-  }
   // Uses MainView's state to control its UI.
   render() {
-    const { movies, selectedMovie, user, register } = this.state;
-    /* If there is no user, and is registered the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user && register) return [
-      < HeaderView />,
-      <LoginView onLoggedIn={user => this.onLoggedIn(user)} onRegister={register => this.onRegister(register)} />
-    ]
-    /* If there is no user and no register , the RegistrationView  is rendered. */
-    if (!user && !register) return [
-      < HeaderView />,
-      <RegistrationView onRegister={register => this.onRegister(register)} />
-    ]
-
-    // Before the movies have been loaded.
-    if (movies.length === 0) return <div className="main-view" />;
-
+    const { movies, user } = this.state;
     return (
-      <div>
-        {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned */}
-        <HeaderView />
-        {selectedMovie
-          ? (
-            //<Row className="justify-content-md-center ">
-            <Row className="d-flex flex-column justify-content-between align-items-center ">
+      <Router>
+        {/*
+        <div>
+          <HeaderView />
+        </div>
+         */}
+        <Navbar className="navbar-style" bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img src={imageurl} alt="logo"
+              width="100"
+              className="d-inline-block align-top"></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href={`/users/${user}`}>Profile</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          {/*<Navbar.Text>
+          Signed in as: <a href="#login">username</a>
+        </Navbar.Text>*/}
+          <Button className="button-style btn-sm" onClick={() => { this.onLoggedOut() }}>Logout</Button>
+        </Navbar >
 
-              <Col xs={12} sm={10} md={8} lg={6} className="d-flex flex-column align-items-center">
-                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+        {/* <Row className="main-view justify-content-md-center"> */}
+        <Row className="d-flex justify-content-center align-items-center mx-1">
+
+          <Route exact path="/" render={() => {
+            if (!user) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return movies.map(m => (
+              <Col xs={12} sm={6} md={4} lg={3} xl={2} key={m._id} className="d-flex flex-column align-items-center">
+                <MovieCard movieData={m} />
               </Col>
-            </Row>
-          )
-          : (
-            <Row className="d-flex justify-content-start align-items-center mx-1">
-              {movies.map(movie => (
-                <Col xs={12} sm={6} md={4} lg={3} xl={2} key={movie._id} className="d-flex flex-column align-items-center" >
-                  <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
-                </Col>
-              ))}
-            </Row>
-          )}
-      </div>
+            ))
+          }} />
+          <Route path="/register" render={() => {
+            if (user) return <Redirect to="/" />
+            return <Col>
+              <RegistrationView />
+            </Col>
+          }} />
+
+          <Route path="/users/:username" render={() => {
+            if (!user) return;
+            <Col>
+              <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+            </Col>;
+            return (
+              <Col>
+                <ProfileView
+                  onLoggedIn={(user) => this.onLoggedIn(user)}
+                  movies={movies}
+                  user={user}
+                  onBackClick={() => history.goBack()}
+                />
+              </Col>
+            );
+          }}
+          />
+          <Route path="/movies/:movieId" render={({ match, history }) => {
+            if (!user) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col xs={12} sm={10} md={8} lg={6} className="d-flex flex-column align-items-center p-0">
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+            </Col>
+          }} />
+
+          <Route path="/directors/:name" render={({ match, history }) => {
+            if (!user) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+            </Col>
+          }
+          } />
+
+          <Route path="/genres/:name" render={({ match, history }) => {
+            if (!user) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+            </Col>
+          }
+          } />
+        </Row>
+      </Router>
     );
+
+    //<div>
+    // {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned */}
+    //<HeaderView />
+    //{selectedMovie
+    // ? (
+    //<Row className="justify-content-md-center ">
+    //  <Row className="d-flex flex-column justify-content-between align-items-center ">
+
+    // <Col xs={12} sm={10} md={8} lg={6} className="d-flex flex-column align-items-center">
+    //  <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+    // </Col>
+    // </Row>
+    //)
+    // : (
+    //  <Row className="d-flex justify-content-start align-items-center mx-1">
+    //   {movies.map(movie => (
+    //     <Col xs={12} sm={6} md={4} lg={3} xl={2} key={movie._id} className="d-flex flex-column align-items-center" >
+    //       <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+    //     </Col>
+    //   ))}
+    //  </Row>
+    //)}
+    //</div>
+
   }
 }
