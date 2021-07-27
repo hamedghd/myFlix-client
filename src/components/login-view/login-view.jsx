a4bfdb358783b40b4d3cee132309a102dddb6a56
hamedghd <h.gharibdoost@gmail.com>
2021-07-27 22:46:25 +0200

    Add form validation.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -11,9 +11,15 @@ import './login-view.scss';
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
     console.log(username, password);
     /* Send a request to the server for authentication */
     axios.post('https://myflix-movieapi.herokuapp.com/login', {
@@ -30,21 +36,25 @@ export function LoginView(props) {
         alert('User or password is incorrect!');
         console.error(e);
       });
+    setValidated(true);
+
   };
 
   return (
     <Row className="justify-content-md-center">
       <Col md={6} lg={4} className="login-style d-flex flex-column align-items-center text-center bg-dark p-2">
         <h1 className="title">Log in to myFlix!</h1>
-        <Form>
+        <Form noValidate validated={validated}>
           <Form.Group controlId="formUsername">
             <Form.Label>Username:</Form.Label>
             <Form.Control type="text" onChange={e => setUsername(e.target.value)} required />
+            <Form.Control.Feedback type="invalid"> Please insert your username. </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formPassword">
             <Form.Label>Password:</Form.Label>
             <Form.Control type="password" onChange={e => setPassword(e.target.value)} required />
+            <Form.Control.Feedback type="invalid"> Please insert your password.</Form.Control.Feedback>
           </Form.Group>
 
           <Button className="button-style" variant="primary" type="submit" onClick={handleSubmit}>
