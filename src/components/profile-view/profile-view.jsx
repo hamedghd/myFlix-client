145398334b70cd4ad790d699404b1aaa40b2ef52
hamedghd <h.gharibdoost@gmail.com>
2021-07-27 14:53:36 +0200

    Add form validation

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -20,7 +20,8 @@ export class ProfileView extends React.Component {
       Password: "",
       Email: "",
       Birthday: "",
-      FavoriteMovies: []
+      FavoriteMovies: [],
+      validated: false
     };
   }
 
@@ -83,11 +84,14 @@ export class ProfileView extends React.Component {
 
   handleUpdateAccount = (e) => {
     e.preventDefault();
-
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
     // Credentials
     const user = localStorage.getItem('user');
     const token = localStorage.getItem('token');
-
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -123,6 +127,9 @@ export class ProfileView extends React.Component {
         console.log(e);
         //console.log(e.res.data);
       });
+    this.setState({
+      validated: true,
+    });
   };
 
   render() {
@@ -135,25 +142,33 @@ export class ProfileView extends React.Component {
         <Col md={6} lg={4} className="profile-box bg-dark">
           <h1 className="title">Update your profile information!</h1>
 
-          <Form className="">
+          <Form noValidate validated={this.state.validated}>
             <Form.Group controlId="formUsername" >
               <Form.Label>Username:</Form.Label>
               <Form.Control type="text" name="Username" placeholder="New username" onChange={this.handleInputChange} required />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid"> Please choose a username. </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="formPassword" >
               <Form.Label>Password:</Form.Label>
               <Form.Control type="password" name="Password" placeholder="New password" onChange={this.handleInputChange} required minLength="5" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid"> Please choose a valid password. (minimum length = 5)</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="formEmail" >
               <Form.Label>Email:</Form.Label>
               <Form.Control type="email" name="Email" placeholder="New email" onChange={this.handleInputChange} required />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid"> Please choose a valid Email address.</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="formBirthday" >
               <Form.Label>Birthday:</Form.Label>
               <Form.Control type="date" name="Birthday" placeholder="New birthday" onChange={this.handleInputChange} required />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid"> Please choose a valid date.</Form.Control.Feedback>
             </Form.Group>
 
             <Button className="button-style" variant="primary" type="submit" onClick={this.handleUpdateAccount}>
